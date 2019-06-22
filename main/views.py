a1f2eb29809fc8ede34be737adb867614debff2e
Suvansh Rana <suvanshrana94.com>
2019-06-22 14:44:37 +0530

    2

--- main/views.py
+++ main/views.py
@@ -1,8 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import forms as custom_forms
 
 # Create your views here.
 
+def convert(email):
+
+    i = email.index("@")
+    first = email[:i]
+    last = email[i+1:]
+    f = str(idna.encode(first))
+    f = f[2:len(f)-1]
+    s=str((idna.encode(last)))
+    s = s[2:len(s)-1]
+
+    return(f+"@"+s)
+
+def reconvert(email):
+
+    i = email.index("@")
+    first = email[:i]
+    last = email[i+1:]
+
+    f = str(idna.decode(first))
+    s = str((idna.decode(last)))
+
+    return(f+"@"+s)
+
 def index(request):
-    return render(request, "main/index.html")
+
+    form = custom_forms.RegisterForm()
+
+    if request.method == "POST":
+        form = custom_forms.RegisterForm(request.POST)
+
+        if form.is_valid():
+
+            return render(request, "main/home.html", {
+                'name': form.cleaned_data['name'],
+                'email': form.cleaned_data['email'],
+            })
+        
+        else:
+            return HttpResponse("Error")
+
+    return render(request, "main/index.html", {
+        'form': form
+    })
 
