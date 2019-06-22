dee09de8210cdade88af2efb6bcf9d18ebf14087
Suvansh Rana <suvanshrana94.com>
2019-06-22 14:15:33 +0530

    Home pagechanged

--- UA/settings.py
+++ UA/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'UA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/views.py
+++ main/views.py
@@ -4,6 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    
-    return HttpResponse("<h1>At Index</h1>")
+    return render(request, "main/index.html")
 
