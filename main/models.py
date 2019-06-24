ec3f509dbadf3d8fa97c4f3fbd29b1022d9f4294
Aayush Bisen <aayush23101998@outlook.com>
2019-06-24 20:33:18 +0530

    Completed email validation, updated readme

--- main/models.py
+++ main/models.py
@@ -17,28 +17,44 @@ class Person(models.Model):
         i = self.email.index("@")
 
         first = self.email[:i]
-        last = self.email[i+1:]
+        last = self.email[i + 1 :]
 
         f = str(idna.decode(first))
         s = str((idna.decode(last)))
 
-        return(f+"@"+s)
+        return f + "@" + s
 
+    def is_valid_email(self, email):
+
+        parts = self.email.split("@")
+
+        if len(parts) == 2 and all(parts):
+
+            second_part = parts[1]
+
+            pieces = second_part.split(".")
+
+            if len(pieces) >= 2 and all(pieces):
+
+                return True
 
     def save(self, *args, **kwargs):
 
         i = self.email.index("@")
         first = self.email[:i]
-        last = self.email[i+1:]
-        
-        f = str(idna.encode(first))
-        f = f[2:len(f)-1]
-        s=str((idna.encode(last)))
-        s = s[2:len(s)-1]
+        last = self.email[i + 1 :]
 
+        f = str(idna.encode(first))
+        f = f[2 : len(f) - 1]
+        s = str((idna.encode(last)))
+        s = s[2 : len(s) - 1]   
+
+        
         new_email = f + "@" + s
 
-        self.email = new_email
-    
-        return super().save(*args, **kwargs)
-        
+        if self.is_valid_email(new_email):
+            self.email = new_email
+            return super().save(*args, **kwargs)
+        else:
+            raise ValueError
+
--- main/views.py
+++ main/views.py
@@ -5,6 +5,7 @@ from .models import Person
 
 # Create your views here.
 
+
 def index(request):
 
     error = False
@@ -16,32 +17,22 @@ def index(request):
 
         if form.is_valid():
 
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
 
             try:
-                p = Person.objects.create(
-                    name=name,
-                    email=email,
-                    password=password
-                )
-
+                p = Person.objects.create(name=name, email=email, password=password)
                 p.save()
 
-            except:
+            except ValueError:
                 error = True
 
             if not error:
-                return render(request, "main/home.html", {
-                    'person': p,
-                })
-        
+                return render(request, "main/home.html", {"person": p})
+
         else:
             return HttpResponse("Error")
 
-    return render(request, "main/index.html", {
-        'form': form,
-        'error': error,
-    })
+    return render(request, "main/index.html", {"form": form, "error": error})
 
