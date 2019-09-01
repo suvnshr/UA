aaea7608401c13204f03fe131325debd8ffd3da9
Suvansh Rana <suvanshrana94.com>
2019-09-01 16:45:00 +0530

    Css file added, base template added, setiings changed, .gitignore updated

--- UA/settings.py
+++ UA/settings.py
@@ -125,5 +125,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    "/main/static/"
+]
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
